53cff1f7afc666c6198c6ea5f42046d505f3fb30
Mohamed Hesham <0mohamedhisham@gmail.com>
2026-08-24 13:07:17 +0300

    secound commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tanstackStart({ customViteReactPlugin: true }), tsconfigPaths(), react(), tailwindcss()],
+  plugins: [
+    tanstackStart({ customViteReactPlugin: true }),
+    nitro({ preset: "vercel" }),
+    tsconfigPaths(),
+    react(),
+    tailwindcss(),
+  ],
 });
